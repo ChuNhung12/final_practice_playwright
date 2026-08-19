df5c98a7ea39df3a35f62805f0cf7cb07ee0307f
ChuNhung12 <nhung.chuthihong@nashtechglobal.com>
2026-08-19 15:30:25 +0700

    Update Playwright configuration and dependencies

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,12 +18,15 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['allure-playwright', { outputFolder: 'allure-results' }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: process.env.BASE_URL,
-    headless: process.env.HEADLESS !== 'false',
+    headless: false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     launchOptions: {
